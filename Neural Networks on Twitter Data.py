e0a4ab41aec60f74bdb35de886e9503266b90378
mckinleyharlett <70404362+mckinleyharlett@users.noreply.github.com>
2021-10-26 13:16:11 -0400

    Update Neural Networks on Twitter Data.py

--- Neural Networks on Twitter Data.py
+++ Neural Networks on Twitter Data.py
@@ -1,9 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
+# McKinley Harlett
 
 # # Neural Network Classifier Exercises
-
-# In[37]:
 
 
 # Packages 
@@ -29,9 +26,6 @@ import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 
-# In[2]:
-
-
 # Loading the data
 data = []
  
@@ -39,53 +33,29 @@ with jsonlines.open('categorized-comments.jsonl') as reader:
     for obj in reader.iter(type=dict, skip_invalid=True):
         data.append(obj)
 
-
-# In[3]:
-
-
 df = pd.DataFrame(data=data)
 df.head()
 
 
 # ### Cleaning the data
 
-# In[10]:
-
 
 # Getting a smaller sample
 df1 = df.sample(n=10000)
 
-
-# In[11]:
-
-
 # Now we are going to be convering all text in the rows to lowercase
 df1['lowertext'] = df1['txt'].apply(lambda x: x.lower())
-
-
-# In[12]:
-
 
 # Removing any unnecessary punctuation 
 punctuation = dict.fromkeys(i for i in range(sys.maxunicode)
                             if unicodedata.category(chr(i)).startswith('P')) 
 
-
-# In[13]:
-
-
 # Applying the punctuation step to our text field
 df1['lowertext'] = [string.translate(punctuation) for string in df1['lowertext']]
 
 
-# In[14]:
-
-
 # Tokenizing the words from each row
 tokenized_words = df1.apply(lambda row: nltk.word_tokenize(row['lowertext']), axis=1)
-
-
-# In[43]:
 
 
 # I need to make the category column into a numerical
@@ -95,24 +65,13 @@ df1['code'] = df1['cat'].cat.codes
 
 # # 1. Neural Network Classifier with Scikit
 
-# In[44]:
-
-
 # Generating TFIDF values 
 tfidfconverter = TfidfVectorizer(max_features = 50000, stop_words = stopwords.words('english'))
 
 X = tfidfconverter.fit_transform(df1['lowertext']).toarray()
 
-
-# In[45]:
-
-
 # Splitting our data into training and testing datasets
 X_train, X_test, Y_train, Y_test = train_test_split(X, df1['code'].values, test_size=0.25, random_state=0)
-
-
-# In[46]:
-
 
 # Fitting neural network model using our training data
 import warnings
@@ -121,17 +80,9 @@ warnings.filterwarnings('ignore')
 classifier = MLPClassifier(hidden_layer_sizes = (30,30,30), max_iter = 500, random_state=0)
 classifier.fit(X_train, Y_train)
 
-
-# In[47]:
-
-
 # Predicting the comment categories by comparing the test category
 Y_predict = classifier.predict(X_test)
 Y_predict
-
-
-# In[55]:
-
 
 # Printing results
 print('Accuracy: \n {} \n'.format(accuracy_score(Y_test, Y_predict)))
@@ -141,9 +92,6 @@ print('Confusion Matrix: \n {} \n'.format(confusion_matrix(Y_test, Y_predict)))
 
 # # 2. Neural Network Classifier with Keras
 
-# In[49]:
-
-
 # Packages
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
@@ -152,9 +100,6 @@ from numpy import loadtxt
 from keras.models import Sequential
 from keras.layers import Dense
 from keras import layers
-
-
-# In[50]:
 
 
 # Creating a function so then it'll be faster and easier for me to understand
@@ -187,23 +132,11 @@ def train_test_model(X_train, Y_train, X_test):
     return y_pred
 
 
-# In[51]:
-
 
 # Fit model to our training data!
 y_pred = train_test_model(X_train, Y_train, X_test)
 
 
-# In[56]:
-
-
 print('Accuracy: \n {} \n'.format(accuracy_score(Y_test, y_pred)))
 print('Confustion Matrix: \n {} \n'.format(confusion_matrix(Y_test, y_pred)))
 print('Classification Report: \n {} \n'.format(classification_report(Y_test, y_pred)))
-
-
-# In[ ]:
-
-
-
-
